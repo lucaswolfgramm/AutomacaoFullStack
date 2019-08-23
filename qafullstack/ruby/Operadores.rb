fd66eb2e83c53a1ee2a20f1f1a659219ec737043
Lucas Wolfgramm <lucas.wolfgramm@softexpert.com>
2019-08-23 16:36:46 -0300

    Inicio Ruby 23.08

---
+++ qafullstack/ruby/Operadores.rb
@@ -0,0 +1,14 @@
+numero1 = 0
+numero2 = 0
+total = 0
+
+puts "Informe o número 1:"
+numero1 = gets.chomp.to_i
+
+puts "Informe o número 2:"
+numero2 = gets.chomp.to_i
+
+total = numero1 + numero2
+
+puts 'O valor de total número 1 + número 2 é: '
+puts total
