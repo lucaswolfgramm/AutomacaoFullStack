d6ad03718e09a9f612d846e842cd60562d0036ab
Lucas Wolfgramm <lucas.wolfgramm@softexpert.com>
2019-08-27 13:53:17 -0300

    Operadores, Hashes, loops e metodos

---
+++ qafullstack/ruby/metodos.rb
@@ -0,0 +1,20 @@
+def diga_ola(nome)
+  puts "Olá, " + nome
+end
+
+diga_ola("Lucas")
+
+# def retorna_nome
+#   "Lucas"
+# end
+
+# resultado = retorna_nome
+# puts resultado
+
+# def soma(v1, v2)
+#   total = v1 + v2
+#   total
+# end
+
+# res = soma(5, 10)
+# puts res
