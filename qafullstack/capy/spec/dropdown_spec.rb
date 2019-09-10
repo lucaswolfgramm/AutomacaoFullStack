68f590b19a29dc3cda694f8637c61a6534cba100
Lucas Wolfgramm <lucas.wol@hotmail.com>
2019-09-09 23:00:23 -0300

    Conhecendo o Capybara 4 ao 7

---
+++ qafullstack/capy/spec/dropdown_spec.rb
@@ -0,0 +1,22 @@
+
+describe "Caixa de seleção", :dropdown do
+  it "Item específico simples" do
+    visit "/dropdown"
+    select("Loki", from: "dropdown")
+    sleep 3
+  end
+
+  it "Item específico com find" do
+    visit "/dropdown"
+    dropList = find(".avenger-list")
+    dropList.find("option", text: "Scott Lang").select_option
+    sleep 3
+  end
+
+  it "Qualquer item", :sample do
+    visit "/dropdown"
+    dropList = find(".avenger-list")
+    dropList.all("option").sample.select_option
+    sleep 3
+  end
+end
--- qafullstack/capy/spec/forms_spec.rb
+++ qafullstack/capy/spec/forms_spec.rb
@@ -1,6 +1,32 @@
-describe "Forms" do
+def login(user, password)
+  visit "/login"
 
-    it "Login com Sucesso" do
-        visit "https://training-wheels-protocol.herokuapp.com/login"
-    end
+  fill_in "userId", with: user
+  fill_in "password", with: password
+  click_button "Login"
+end
+
+describe "Forms" do
+  it "Login com Sucesso" do
+    login("stark", "jarvis!")
+
+    expect(find("#flash").visible?).to be true
+
+    #expect(find("#flash").text).to include "Olá, Tony Stark. Você acessou a área logada!"
+    expect(find("#flash")).to have_content "Olá, Tony Stark. Você acessou a área logada!"
+  end
+
+  it "senha incorreta" do
+    login("stark", "jarvis")
+
+    expect(find("#flash").visible?).to be true
+    expect(find("#flash")).to have_content "Senha é invalida!"
+  end
+
+  it "usuário não cadastrado" do
+    login("lucas", "teste")
+
+    expect(find("#flash").visible?).to be true
+    expect(find("#flash")).to have_content "O usuário informado não está cadastrado!"
+  end
 end
